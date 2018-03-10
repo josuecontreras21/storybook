acafa7b69344a83de9337685607c861357ab4a57
Josue Contreras <josue.contreras21@gmail.com>
2018-03-10 17:41:28 -0400

    add random message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport'); 
+//radom comment
 
 // Passport config
 require('./config/passport')(passport);

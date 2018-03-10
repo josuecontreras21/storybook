81de32cbbda07d6b99803119d9c776ce0233f7be
Josue Contreras <josue.contreras21@gmail.com>
2018-03-10 13:53:40 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res)=>{res.send('home page..')})
+
+
+app.listen(port, 'localhost', ()=>console.log(`Server started on port ${port}`))
